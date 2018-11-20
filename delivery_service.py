80d499db293b80ae584c2d062d74085717cb7696
Nikhil <ennsharma@berkeley.edu>
2018-11-20 15:12:10 -0800

    Minor fixes

--- delivery_service.py
+++ delivery_service.py
@@ -9,7 +9,7 @@ import random
 import sched, time
 
 class ScrapingHTTPServer(HTTPServer):
-    SCRAPE_INTERVAL = 5 # scrape USPS once every 5 seconds
+    SCRAPE_INTERVAL = 10 # scrape USPS once every 5 seconds
     ERC20_ABI = json.loads('[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_owner","type":"address"},{"indexed":true,"name":"_spender","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Approval","type":"event"}]')
 
     def __init__(self, server_address, handler_class):
@@ -82,7 +82,7 @@ class Server(BaseHTTPRequestHandler):
             return
         
         # add the tracking number to the list of packages we track
-        self.server.tracked_packages[tracking_number] = w3.eth.contract(address=contract_address, abi=ERC20_ABI)
+        self.server.tracked_packages[tracking_number] = w3.eth.contract(address=contract_address, abi=self.server.ERC20_ABI)
 
         # send response
         self._set_headers()
